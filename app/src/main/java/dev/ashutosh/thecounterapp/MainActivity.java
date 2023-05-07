74b8a2b057c9157aa9f0df072d8776c2efe2c53f
Ashutosh Dubey <ashutoshdubey133@gmail.com>
2023-05-08 00:44:21 +0530

    Added Functionality

--- app/src/main/java/dev/ashutosh/thecounterapp/MainActivity.java
+++ app/src/main/java/dev/ashutosh/thecounterapp/MainActivity.java
@@ -3,12 +3,29 @@ package dev.ashutosh.thecounterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView counterTextView;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        counterTextView = findViewById(R.id.counterText);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer counterInteger = Integer.parseInt(counterTextView.getText().toString());
+                counterTextView.setText(String.valueOf(counterInteger+1));
+            }
+        });
     }
 }
