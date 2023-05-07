6256f84ed19c03c14f48606bfe7861616650026d
Abhishek <abhishek-at-213445775584>
2023-05-07 23:38:44 +0530

    Initial Commit

---
+++ app/src/main/java/dev/ashutosh/thecounterapp/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.ashutosh.thecounterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
